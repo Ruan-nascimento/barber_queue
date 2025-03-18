97bd0eab6dd6f2c31d79b1838695a274703a9505
Ruan Carlos <ruan.nascimento019@gmail.com>
2025-03-18 13:48:54 -0300

    update: api-acept-route.ts

--- src/app/api/accept/[id]/route.ts
+++ src/app/api/accept/[id]/route.ts
@@ -1,13 +1,37 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
+
 const prisma = new PrismaClient();
 
-export async function POST(req: NextRequest, { params }: { params: { id: string } }) {
-  await prisma.client.update({
-    where: { id: params.id },
-    data: { status: "queue" },
-  });
 
-  return NextResponse.json({ success: true });
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    if (!params.id) {
+      return NextResponse.json(
+        { error: "ID do cliente é requerido" },
+        { status: 400 }
+      );
+    }
+
+    const updatedClient = await prisma.client.update({
+      where: { id: params.id },
+      data: { status: "queue" },
+    });
+
+    return NextResponse.json({ success: true }, { status: 200 });
+  } catch (error) {
+    console.error("Erro ao atualizar cliente:", error); 
+    return NextResponse.json(
+      { error: "Erro ao aceitar o cliente" },
+      { status: 500 }
+    );
+  }
 }
+
+process.on("SIGTERM", async () => {
+  await prisma.$disconnect();
+});
