02246f77f89be63cb85dcedb9a3b77b62d9b0d54
Ruan Carlos <ruan.nascimento019@gmail.com>
2025-03-18 14:14:01 -0300

    update: api-acept-route.ts

--- src/app/api/accept/[id]/route.ts
+++ src/app/api/accept/[id]/route.ts
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 
 export async function POST(
   req: NextRequest,
-  context: { params: { id: any } }
+  context: { params: any }
 ) {
   try {
     const { id } = context.params; // Extraindo o ID corretamente
